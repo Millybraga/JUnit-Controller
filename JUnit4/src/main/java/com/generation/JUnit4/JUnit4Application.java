d722739cb1b521d24bcd1eb864f0e3e024a9a3ee
Millybraga <emilly.braga02@hotmail.com>
2021-05-17 15:25:22 -0300

    teste unitario controller

---
+++ JUnit4/src/main/java/com/generation/JUnit4/JUnit4Application.java
@@ -0,0 +1,13 @@
+package com.generation.JUnit4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnit4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUnit4Application.class, args);
+	}
+
+}
---
+++ JUnit4/src/test/java/com/generation/JUnit4/JUnit4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.JUnit4;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnit4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
